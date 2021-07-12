6ea2107a9c01bf6ff40044356504806884d35fad
ישי מאיה <ssaymama@gmail.com>
2021-07-12 16:37:11 +0300

    show sub data on screen

--- push.js
+++ push.js
@@ -8,7 +8,7 @@ const vapIdKeys = {
 
 console.log(vapIdKeys);
 
-push.setVapidDetails('Hello subject',vapIdKeys.publicKey, vapIdKeys.privateKey);
+push.setVapidDetails('mailto:ssaymama@gmail.com:',vapIdKeys.publicKey, vapIdKeys.privateKey);
 
-const sub = {};
+const sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/eJ4mzYnOg4U:APA91bG2eixzUb5OojbkbApFdHCeOHk4nXV_mAwQ1UNUla0oBGqWMy8bz3Itz8zMrPiVRtqEY4Q7bwdYanvApqadWSEOm7KZEIawGHLCguvmDm1mEJ50nJyPRBPyhh3gJ7eHMb-1Ecfp","expirationTime":null,"keys":{"p256dh":"BCWRPcfBkLe8_rzJOkxMPZ4SFjZi7QezmE7p8Uv9n41FS_3DCmCRRXc6Z-7ZrsCEyh2237nWZJWlOmkGJByE3Vc","auth":"muTTZqj6Bjs4LUW5njnuGw"}}
 push.sendNotification(sub, 'test message')
